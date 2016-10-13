3f8e1f30a2a612633d62f485c013c49717967799
wb-lcn221302 <wb-lcn221302@alibaba-inc.com>
2016-10-13 17:42:51 +0800

    second

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,80 @@
-var fs = require('fs'),
-    textarea = document.getElementsByTagName('textarea')[0],
-    button = document.getElementsByTagName('button')[0];
-
-function writeFile() {
+var fs = require('fs');
+/*function writeFile() {
     var text = textarea.value;
     console.log(text);
     fs.writeFileSync('./app/message.txt',
         text, 'utf8');
+}*/
+
+
+var editBtn = $("#edit"),
+     preview = $('#preview'),
+     editBox = $('#editBox'),
+     previewBox = $('#previewBox');
+
+editBtn.on("click",function(){
+    previewBox.hide();
+    editBox.show();
+})
+
+preview.on("click",function(){
+    previewBox.show();
+    editBox.hide();
+    populateStorage();
+    firstFunc();
+})
+
+
+function firstFunc(){
+        $(".J_tb_firstArea .title").text(localStorage.getItem('inputTitle'));
+        $(".J_tb_firstArea .person").text(localStorage.getItem('shareperson'));
+
+        $(".shareContext dd").each(function(index,item){
+            var name = 'shareP'+index;
+            $(".J_tb_share tr").eq(index).find("td").text(localStorage.getItem(name))
+        })
+
+        $(".shareQuestion dd").each(function(index,item){
+            var name = 'shareQ'+index;
+            $(".J_tb_shareQ tr").eq(index).find("td").text(localStorage.getItem(name))
+        })
 }
 
-button.onclick = writeFile;
+if(!localStorage.getItem('inputTitle')) {
+    populateStorage();
+} else {
+    setStyles();
+}
+
+function setStyles() {
+    var title = localStorage.getItem('inputTitle');
+    var person = localStorage.getItem('shareperson');
+    $('#inputTitle').val(title);
+    $('#inputJia').val(person);
+
+    $(".shareContext dd").each(function(index,item){
+        var name = 'shareP'+index;
+        $(item).find("textarea").val(localStorage.getItem(name))
+    })
+    $(".shareQuestion dd").each(function(index,item){
+        var name = 'shareQ'+index;
+        $(item).find("textarea").val(localStorage.getItem(name))
+    })
+}
+
+function populateStorage() {
+    localStorage.setItem('inputTitle', document.getElementById('inputTitle').value);
+    localStorage.setItem('shareperson', document.getElementById('inputJia').value);
+    //localStorage.setItem('image', document.getElementById('image').value);
+    //localStorage.setItem('shareP1', $(".shareContext dd textarea").val());
+    $(".shareContext dd").each(function(index,item){
+        var name = 'shareP'+index;
+        localStorage.setItem(name, $(item).find("textarea").val());
+    })
+    $(".shareQuestion dd").each(function(index,item){
+        var name = 'shareQ'+index;
+        localStorage.setItem(name, $(item).find("textarea").val());
+    })
+
+    setStyles();
+}
--- main.js
+++ main.js
@@ -10,10 +10,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 840, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/hello/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
